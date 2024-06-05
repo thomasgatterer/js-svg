be94b43c85ce89425710bd364a76dffc8e06a929
Thomas Gatterer <t.gatterer@tsn.at>
2024-06-05 17:12:11 +0200

    animate und Funktion ergänzt

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ const hoursElement = document.getElementById("hour_hand");
 const minutesElement = document.getElementById("minute_hand");
 const secondsElement = document.getElementById("second_hand");
 
-const date = new Date();
 
-const hour = date.getHours();
-const minute = date.getMinutes();
-const second = date.getSeconds();
+function animate() {
+	const date = new Date();
 
-hoursElement.setAttribute("transform", `rotate(${(360 / 12) * hour})`);
-minutesElement.setAttribute("transform", `rotate(${(360 / 60) * minute})`);
-secondsElement.setAttribute("transform", `rotate(${(360 / 60) * second})`);
+	const hour = date.getHours();
+	const minute = date.getMinutes();
+	const second = date.getSeconds();
 
+	hoursElement.setAttribute("transform", `rotate(${(360 / 12) * hour})`);
+	minutesElement.setAttribute("transform", `rotate(${(360 / 60) * minute})`);
+	secondsElement.setAttribute("transform", `rotate(${(360 / 60) * second})`);
+	
+	requestAnimationFrame(animate);
+}
+
+requestAnimationFrame(animate);
